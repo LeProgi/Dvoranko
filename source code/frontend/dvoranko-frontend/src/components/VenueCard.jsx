cc8de9b053431da5ad8e68b39137e694a96c92ef
EvaHuljaj <eh55682@fer.hr>
2025-11-03 15:18:35 +0100

    Osnovni izgled stranice
    Napravljen početni izgled stranice i organizacija React projekta, oglasna ploča te popis dvorana i stranica za prikaz podataka o svakoj dvorani, potrebno dodati login pod prijavi se te maps api pod karta

---
+++ source code/frontend/dvoranko-frontend/src/components/VenueCard.jsx
@@ -0,0 +1,17 @@
+const VenueCard = () => {
+    return (
+        <button className="w-11/12">
+            <div className="flex items-center bg-[#e5e5e5] h-20 w-full rounded-lg shadow-sm hover:bg-[#d0d0d0] transition">
+                <div className="w-14 h-14 bg-white rounded-md ml-6"></div>
+
+                {/* Tekst ili info desno */}
+                <div className="ml-6 text-left">
+                    <h3 className="text-lg font-semibold text-gray-700">Naziv dvorane</h3>
+                    <p className="text-sm text-gray-500">Kratki opis ili lokacija</p>
+                </div>
+            </div>
+        </button>
+    );
+};
+
+export default VenueCard;
---
+++ source code/frontend/dvoranko-frontend/src/pages/EventBoard.jsx
@@ -0,0 +1,41 @@
+import Button from "../components/Button";
+import VenueCard from "../components/VenueCard";
+import { Link } from "react-router-dom";
+const EventBoard = () => {
+    return (
+        <div className="flex flex-col min-h-screen w-full bg-gray-100 items-center">
+        
+              <div className="bg-[#3B5B80] w-full pb-10 rounded-b-[40%] flex flex-col items-center text-center">
+                
+                <div className="flex justify-evenly gap-12 mt-4 w-3/4">
+                    <Link to="/" className="w-[50vw] block">
+                        <Button variant="default" title="Početna stranica" />
+                    </Link>
+                    <Button variant="default" title="Karta" />
+                    <Button variant="default" title="O nama" />
+                    <Button variant="default" title="Prijavi se" />
+                    <Button variant="profile" title="profile"/>
+                </div>
+        
+                <h2 className="text-4xl text-white mt-10 mb-10 font-semibold tracking-wide">Oglasna ploča</h2>
+            </div>
+            <div className="flex flex-col w-3/4 bg-[#d9d9d9] rounded-[10px] items-center py-6 mt-12">
+                <h2 className="text-xl font-semibold mb-6">Popis javnih događanja</h2>
+                <div className="flex flex-col items-center gap-4 w-full">
+                    <Link to="/venue1" className="w-11/12 block">
+                        <VenueCard></VenueCard>
+                    </Link>
+                    <Link to="/venue1" className="w-11/12 block">
+                        <VenueCard></VenueCard>
+                    </Link>
+                    <Link to="/venue1" className="w-11/12 block">
+                        <VenueCard></VenueCard>
+                    </Link>
+                    
+                </div>
+        </div>
+        </div>
+    );
+};
+
+export default EventBoard;
