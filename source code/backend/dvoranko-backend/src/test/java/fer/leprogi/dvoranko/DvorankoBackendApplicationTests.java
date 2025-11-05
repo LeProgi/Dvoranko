84a058cf961ac88dee88f9bdc3160290b3dc52f0
IvanVucakGit <iv56203@fer.hr>
2025-11-05 23:08:27 +0100

    uredeni fileovi za deployment

--- source code/backend/dvoranko-backend/src/test/java/fer/leprogi/dvoranko/DvorankoBackendApplicationTests.java
+++ source code/backend/dvoranko-backend/src/test/java/fer/leprogi/dvoranko/DvorankoBackendApplicationTests.java
@@ -1,13 +1,7 @@
 package fer.leprogi.dvoranko;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DvorankoBackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
