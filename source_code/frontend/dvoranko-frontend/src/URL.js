e5868eb9733108672343d1e5d93515462bcd46d1
Vid Martin <150678004+VML17@users.noreply.github.com>
2025-11-12 16:01:44 +0100

    deploy fix urls

---
+++ source_code/frontend/dvoranko-frontend/src/URL.js
@@ -0,0 +1,3 @@
+export const urlLocal = "http://localhost:8080";
+export const urlPublic = "https://dvoranko.onrender.com";
+
--- source_code/frontend/dvoranko-frontend/src/main.jsx
+++ source_code/frontend/dvoranko-frontend/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { urlPublic as url } from './URL.js'
+
+export { url };
 
 createRoot(document.getElementById('root')).render(
     <App />
