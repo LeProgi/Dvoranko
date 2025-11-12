5989bab979a6854a95f6feb60d461599d80721c7
IvanVucakGit <iv56203@fer.hr>
2025-11-13 00:49:23 +0100

    Update FrontendController.java

--- source_code/backend/dvoranko-backend/src/main/java/fer/leprogi/dvoranko/controller/FrontendController.java
+++ source_code/backend/dvoranko-backend/src/main/java/fer/leprogi/dvoranko/controller/FrontendController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FrontendController {
 
-    @RequestMapping(value = { "/", "/{path:^(?!api|static|.*\\..*).*}/**" })
+    @RequestMapping(value = {
+            "/{path:^(?!api|assets|.*\\..*).*}",
+            "/**/{path:^(?!api|assets|.*\\..*).*}"
+    })
     public String forward() {
         return "forward:/index.html";
     }
