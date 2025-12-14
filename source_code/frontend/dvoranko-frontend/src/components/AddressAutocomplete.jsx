2cf7a9dc4eb786a72ad9f3596a7a894a0d11adb5
Vid Martin <150678004+VML17@users.noreply.github.com>
2025-12-14 12:32:27 +0100

    adress autocomplete
    forma za kreiranje dvorane i autocomplete adresa za hrv

---
+++ source_code/frontend/dvoranko-frontend/src/components/AddressAutocomplete.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"; 
+import Autocomplete from "react-google-autocomplete";
+
+function AddressAutocomplete({onSelect, onInvalid}) {
+    const [selectedAddress, setSelectedAddress] = useState("");
+
+    return (
+        <Autocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+            options = {{
+                types: ['address'],
+                componentRestrictions: { country: "hr" },
+            }}
+
+            onPlaceSelected={(place) => {
+                setSelectedAddress(place.formatted_address);
+                onSelect(place);
+            }}
+
+            onChange={(e) => {
+                const value = e.target.value;
+
+                if (value != selectedAddress) {
+                    onInvalid();
+                }
+            }}
+
+            placeholder="Unesite adresu..."
+            style = {{
+                width: '100%',
+                padding: '8px',
+                fontSize: '40',
+                borderRadius: '4px',
+                fontSize: '16px',
+            }}
+        />
+    );
+}
