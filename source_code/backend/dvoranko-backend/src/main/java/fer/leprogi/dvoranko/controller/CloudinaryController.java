38fe4ed12976b648138943e6fe348bcc1a8b3861
Vid Martin <150678004+VML17@users.noreply.github.com>
2026-01-13 14:12:09 +0100

    basic image upload na cloudinary

---
+++ source_code/backend/dvoranko-backend/src/main/java/fer/leprogi/dvoranko/controller/CloudinaryController.java
@@ -0,0 +1,24 @@
+package fer.leprogi.dvoranko.controller;
+
+import fer.leprogi.dvoranko.service.CloudinaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/public/cloudinary")
+public class CloudinaryController {
+
+    private final CloudinaryService cloudinaryService;
+
+    @PostMapping
+    public String uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
+        return cloudinaryService.upload(file);
+    }
+}
