31f0a2079bd4f099a6d11a32214394659b5b6aaa
IvanVucakGit <iv56203@fer.hr>
2026-01-17 02:14:12 +0100

    Prebačen fetch

--- source_code/frontend/dvoranko-frontend/src/components/Calendar.jsx
+++ source_code/frontend/dvoranko-frontend/src/components/Calendar.jsx
@@ -2,13 +2,40 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import hrLocale from '@fullcalendar/core/locales/hr';
+import {url} from "../main.jsx";
 import "./Calendar.css";
 import { useEffect, useState } from "react";
 
-const Calendar = ({handleDateClick, daysOpen}) => {
+const Calendar = ({handleDateClick, venueId}) => {
+    const [workingDays, setWorkingDays] = useState([]);
+    const [days, setDays] = useState([]);
 
-    const dani = daysOpen.split(";").filter(x => x !== "");
-    const radniDani = dani.map(dan => dan.substring(0, 3));
+    const fetchDvoranu = async (venueId) => {
+        try {
+            const response = await fetch(`${url}/api/public/dvorane/${venueId}`, {
+            method: "GET",
+            credentials: "include"
+            });
+
+            const text = await response.text();
+            const respData = JSON.parse(text);
+            const privremeno = "pon:07-12;sri:05-23;cet:13-20;sub:14-18;";
+            const daysTemp = privremeno.split(";").filter(x => x !== "");
+            const workingDaysTemp = daysTemp.map(dan => dan.substring(0, 3));
+            setDays(daysTemp);
+            setWorkingDays(workingDaysTemp);
+
+            console.log("Response from server:", respData.data);
+            return respData;
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            throw error;
+        }
+    }
+
+    useEffect(() => {
+        fetchDvoranu(venueId);
+    }, [venueId]);
 
     return (
         <FullCalendar
@@ -16,20 +43,20 @@ const Calendar = ({handleDateClick, daysOpen}) => {
             initialView="dayGridMonth"
             height="100%"
             dateClick={(info) => {
-                const dayNames = ["ned", "pon", "uto", "sri", "čet", "pet", "sub"];
+                const dayNames = ["ned", "pon", "uto", "sri", "cet", "pet", "sub"];
                 const day = dayNames[info.date.getDay()];
-                if (!radniDani.includes(day)) return;
-                const terminiDay = dani.find(dan => dan.startsWith(day));
+                if (!workingDays.includes(day)) return;
+                const terminiDay = days.find(dan => dan.startsWith(day));
 
                 handleDateClick(info, terminiDay);
             }}
             locale={hrLocale}
             dayCellContent={(arg) => arg.dayNumberText.replace(".", "")}
             dayCellClassNames={(arg) => {
-                const dayNames = ["ned", "pon", "uto", "sri", "čet", "pet", "sub"];
+                const dayNames = ["ned", "pon", "uto", "sri", "cet", "pet", "sub"];
                 const day = dayNames[arg.date.getDay()];
 
-                if (radniDani.includes(day)) {
+                if (workingDays.includes(day)) {
                     return "open-day";
                 } else {
                     return "closed-day";
--- source_code/frontend/dvoranko-frontend/src/pages/ReservationPage.jsx
+++ source_code/frontend/dvoranko-frontend/src/pages/ReservationPage.jsx
@@ -7,7 +7,6 @@ import Calendar from "../components/Calendar";
 const ReservationPage = () => {
    const { state } = useLocation();
    const venueId = state?.venueId;
-   const [daysOpen, setDaysOpen] = useState([]);
    const [selectedDate, setSelectedDate] = useState(null);
    const [allTimes, setallTimes] = useState(["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"]);
    const [availableTimes, setAvailableTimes] = useState([]);
@@ -25,6 +24,10 @@ const ReservationPage = () => {
    const [brojLjudi, setBrojLjudi] = useState("");
    const [formError, setFormError] = useState("");
 
+   useEffect(() => {
+      fetchDvoranu(venueId);
+   }, [venueId]);
+
    const fetchDvoranu = async (venueId) => {
       try {
          const response = await fetch(`${url}/api/public/dvorane/${venueId}`, {
@@ -35,7 +38,6 @@ const ReservationPage = () => {
          const text = await response.text();
          const respData = JSON.parse(text);
          setKapacitet(respData.data.kapacitet);
-         setDaysOpen(respData.data.daysOpen);
 
          console.log("Response from server:", respData.data);
          return respData;
@@ -43,10 +45,9 @@ const ReservationPage = () => {
          console.error("Error fetching data:", error);
          throw error;
       }
-   }
+   };
 
    const handleDateClick = (info, terminiDay) => {
-      fetchDvoranu(venueId);
       const dateString = String(info.date.getDate()).padStart(2, "0") + "." + String(info.date.getMonth() + 1).padStart(2, "0") + "." + info.date.getFullYear() + ".";
       setSelectedDate(dateString);
       const start = terminiDay.substring(terminiDay.indexOf(":") + 1, terminiDay.indexOf("-"));
@@ -54,7 +55,6 @@ const ReservationPage = () => {
       const startIndex = allTimes.findIndex(time => time.startsWith(start));
       const endIndex = allTimes.findIndex(time => time.startsWith(end));
       setAvailableTimes(allTimes.slice(startIndex, endIndex + 1));
-      //fetch kapacitet
       fetchTakenTimes(dateString);
    };
 
@@ -163,7 +163,7 @@ const ReservationPage = () => {
       <div className="bg-[#5B7692] min-h-screen flex justify-center md:items-center h-auto">
          <div className="flex flex-col md:flex-row justify-between bg-white rounded-[20px] w-[90vw] md:h-[80vh] p-5 h-auto mt-5 md:mt-0 mb-5 md:mb-0">
             <div className="w-[100%] md:w-[50%] h-[550px] md:h-[100%]" >
-               <Calendar handleDateClick={handleDateClick} daysOpen={daysOpen}/>
+               <Calendar handleDateClick={handleDateClick} venueId={venueId}/>
             </div>
 
             <div className="w-[100%] md:w-[49%] h-auto md:h-[100%] flex items-center justify-center">
