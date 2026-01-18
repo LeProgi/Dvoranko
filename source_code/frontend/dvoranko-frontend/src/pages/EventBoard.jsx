f6092482adfef1c299d6fe5b4a43e440786d5731
Vid Martin <150678004+VML17@users.noreply.github.com>
2026-01-18 20:20:45 +0100

    hardcode podaci za oglas

--- source_code/frontend/dvoranko-frontend/src/pages/EventBoard.jsx
+++ source_code/frontend/dvoranko-frontend/src/pages/EventBoard.jsx
@@ -4,6 +4,7 @@ import Button from "../components/Button";
 import VenueCard from "../components/VenueCard";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
+import ReservationCard from "../components/ReservationCard.jsx";
 const EventBoard = () => {
     const [user, setUser] = useState(null);
 
@@ -22,6 +23,17 @@ useEffect(() => {
 const handleGoogleLogin = () => {
   window.location.href = `${url}/oauth2/authorization/google`;
 };
+
+const getDateFromTimestamp = (timestamp) => {
+        try{
+            const date = timestamp.slice(0, 10);
+            const[year, month, day] = date.split("-")
+            return `${day}.${month}.${year}`
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
 
     return (
         <div className="flex flex-col min-h-screen w-full bg-gray-100 items-center">
@@ -62,15 +74,15 @@ const handleGoogleLogin = () => {
             <div className="flex flex-col w-3/4 bg-[#d9d9d9] rounded-[10px] items-center py-6 mt-12 mb-12">
                 <h2 className="text-xl font-semibold mb-6">Popis javnih događanja</h2>
                 <div className="flex flex-col items-center gap-4 w-full">
-                    <Link to="/venue1" className="w-11/12 block">
-                        <VenueCard></VenueCard>
-                    </Link>
-                    <Link to="/venue1" className="w-11/12 block">
-                        <VenueCard></VenueCard>
-                    </Link>
-                    <Link to="/venue1" className="w-11/12 block">
-                        <VenueCard></VenueCard>
-                    </Link>
+                    <ReservationCard 
+                        nameDogadanje={"Interliber"}
+                        opisDogadanje={"Najveca izlozba knjiga u regionu"}
+                        nameDvorana={"Velesajam Zagreb"}
+                        adresa={"Avenija Dubrovnik 15, Zagreb"}
+                        imgUrl={"https://res.cloudinary.com/dnrpd10np/image/upload/v1768420464/dvorane/10/img_1.png"}
+                        vrijemeOd={"2026-01-07 10:00:00"}
+                        vrijemeDo={"2026-01-07 20:00:00"}
+                        ></ReservationCard>
                     
                 </div>
             </div>
